872cdb23bbf6220ad3b406a089f7c6c402acd776
mss0809 <masashiyamamoto0809@gmail.com>
2026-04-30 14:36:37 +0900

    add restricted zone alert

--- prototype/26th_pitchsender_prototype/parameters.cpp
+++ prototype/26th_pitchsender_prototype/parameters.cpp
@@ -1,45 +1,37 @@
 /*------------------------------------
 
 このファイルの役割：変数，定数の宣言
-最終更新：2026/04/11 00:35
-更新内容：胴体桁電装用変数の追加
-注) 各基板において不要な変数も宣言されているが，メモリを余計に消費するため，不要なものはコメントアウトすること．
+最終更新：2026/04/17 17:17
+更新内容：送信用変数の見直し
 
 ------------------------------------*/
-
 
 
 #include <Arduino.h>
 #include "parameters.h"
 
-
-const float const_platform_altitude_m = 10.6f;
-
-//flight_phase, speed_level
-FlightPhase flight_phase = PLATFORM;
-SpeedLevel speed_level = SLOW;
-
-//離陸判定
+// 離陸判定
 volatile bool takeoff = false;
 
-//動作時間
+// 動作時間
 volatile uint32_t time_ms = 0;
 
-// filtered
-volatile float estimated_altitude_lake_m = const_platform_altitude_m;
+// 気圧高度・超音波高度フィルタリング用
+volatile float filtered_bmp_altitude_m = 0.0f;
+volatile float filtered_urm_altitude_m = 0.0f;
 
-//雑に実装．air_xiao用変数
-volatile float data_altitude_bmp_urm_offset_m = 0.0f;
+// 超音波センサを信用するか
+volatile bool urm_is_reliable = false;
 
 
 /*--- エアデータ用 ---*/
 
-//BMP390
+// BMP390
 volatile float data_air_bmp_pressure_hPa = 0.0f;
 volatile float data_air_bmp_temperature_deg = 0.0f;
 volatile float data_air_bmp_altitude_m = 0.0f;
 
-//GPS
+// GPS
 volatile uint8_t data_air_gps_hour = 0;
 volatile uint8_t data_air_gps_minute = 0;
 volatile uint8_t data_air_gps_second = 0;
@@ -49,22 +41,22 @@ volatile double data_air_gps_longitude_deg = 0.0;
 volatile double data_air_gps_altitude_m = 0.0;
 volatile double data_air_gps_groundspeed_ms = 0.0;
 
-//SDP31
+// SDP31
 volatile float data_air_sdp_differentialPressure_Pa = 0.0f;
 volatile float data_air_sdp_airspeed_ms = 0.0f;
 
-//AoA,AoS
+// AoA,AoS
 volatile float data_air_AoA_angle_deg = 0.0f;
 volatile float data_air_AoS_angle_deg = 0.0f;
 
-//ICS基盤
+// ICS基盤
 volatile int data_ics_angle = 0;
 
 
 /*--- 胴体桁電装用 ---*/
 volatile bool psd_is_alive = false;
 
-//BNO055
+// BNO055
 volatile float data_psd_bno_accx_mss = 0.0f;
 volatile float data_psd_bno_accy_mss = 0.0f;
 volatile float data_psd_bno_accz_mss = 0.0f;
@@ -76,18 +68,16 @@ volatile float data_psd_bno_roll = 0.0f;
 volatile float data_psd_bno_pitch = 0.0f;
 volatile float data_psd_bno_yaw = 0.0f;
 
-//BNO055キャリブレーション状態保存用
+// BNO055キャリブレーション状態保存用
 volatile uint8_t data_psd_bno_cal_system = 0;
 volatile uint8_t data_psd_bno_cal_gyro = 0;
 volatile uint8_t data_psd_bno_cal_accel = 0;
 volatile uint8_t data_psd_bno_cal_mag = 0;
 
-//BMP390
+// BMP390
 volatile float data_psd_bmp_pressure_hPa = 0.0f;
 volatile float data_psd_bmp_temperature_deg = 0.0f;
 volatile float data_psd_bmp_altitude_m = 0.0f;
-
-
 
 /*--- Under電装部 ---*/
 volatile bool under_is_alive = false;
--- prototype/26th_pitchsender_prototype/parameters.h
+++ prototype/26th_pitchsender_prototype/parameters.h
@@ -1,53 +1,34 @@
 /*------------------------------------
 
 このファイルの役割：変数，定数の宣言
-最終更新：2026/04/11 00:35
-更新内容：胴体桁電装用変数の追加
-注) 各基板において不要な変数も宣言されているが，メモリを余計に消費するため，不要なものはコメントアウトすること．
+最終更新：2026/04/17 17:17
+更新内容：送信用変数の見直し
 
 ------------------------------------*/
 
 #pragma once  // インクルードガード（複数回読み込まれないようにする）
 
-extern const float const_platform_altitude_m;
-
-//離陸・スピード判定
-enum FlightPhase {
-  PLATFORM,
-  HIGH_LEVEL,
-  MID_LEVEL,
-  LOW_LEVEL
-};
-extern FlightPhase flight_phase;
-
-enum SpeedLevel {
-  FAST,
-  NORMAL,
-  SLOW
-};
-extern SpeedLevel speed_level;
-
-//離陸判定
+// 離陸判定
 extern volatile bool takeoff;
-
-//動作時間
+// 動作時間
 extern volatile uint32_t time_ms;
 
-//filtered
-extern volatile float estimated_altitude_lake_m;
 
-//雑に実装．air_xiao用変数
-extern volatile float data_altitude_bmp_urm_offset_m;
-
+/*--- 高度計算用 ---*/
+// 気圧高度・超音波高度フィルタリング用
+extern volatile float filtered_bmp_altitude_m;
+extern volatile float filtered_urm_altitude_m;
+// 超音波高度を信用するか
+extern volatile bool urm_is_reliable;
 
 /*--- エアデータ用 ---*/
 
-//BMP390
+// BMP390
 extern volatile float data_air_bmp_pressure_hPa;
 extern volatile float data_air_bmp_temperature_deg;
 extern volatile float data_air_bmp_altitude_m;
 
-//GPS
+// GPS
 extern volatile uint8_t data_air_gps_hour;
 extern volatile uint8_t data_air_gps_minute;
 extern volatile uint8_t data_air_gps_second;
@@ -57,22 +38,21 @@ extern volatile double data_air_gps_longitude_deg;
 extern volatile double data_air_gps_altitude_m;
 extern volatile double data_air_gps_groundspeed_ms;
 
-//SDP31
+// SDP31
 extern volatile float data_air_sdp_differentialPressure_Pa;
 extern volatile float data_air_sdp_airspeed_ms;
 
-//AoA,AoS
+// AoA,AoS
 extern volatile float data_air_AoA_angle_deg;
 extern volatile float data_air_AoS_angle_deg;
 
-//ICS基盤
+// ICS基盤
 extern volatile int data_ics_angle;
-
 
 /*--- 胴体桁電装用 ---*/
 extern volatile bool psd_is_alive;
 
-//BNO055
+// BNO055
 extern volatile float data_psd_bno_accx_mss;
 extern volatile float data_psd_bno_accy_mss;
 extern volatile float data_psd_bno_accz_mss;
@@ -84,17 +64,16 @@ extern volatile float data_psd_bno_roll;
 extern volatile float data_psd_bno_pitch;
 extern volatile float data_psd_bno_yaw;
 
-//BNO055キャリブレーション状態保存用
+// BNO055キャリブレーション状態保存用
 extern volatile uint8_t data_psd_bno_cal_system;
 extern volatile uint8_t data_psd_bno_cal_gyro;
 extern volatile uint8_t data_psd_bno_cal_accel;
 extern volatile uint8_t data_psd_bno_cal_mag;
 
-//BMP390
+// BMP390
 extern volatile float data_psd_bmp_pressure_hPa;
 extern volatile float data_psd_bmp_temperature_deg;
 extern volatile float data_psd_bmp_altitude_m;
-
 
 /*--- Under電装部 ---*/
 extern volatile bool under_is_alive;
