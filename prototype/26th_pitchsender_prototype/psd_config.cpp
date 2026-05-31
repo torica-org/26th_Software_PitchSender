ad7bb6f59ff78601d373ca78684641c29143dc5e
mss0809 <masashiyamamoto0809@gmail.com>
2026-05-31 11:08:24 +0900

    before changing repository name

--- prototype/26th_pitchsender_prototype/psd_config.cpp
+++ prototype/26th_pitchsender_prototype/psd_config.cpp
@@ -3,8 +3,8 @@
 // 胴体桁基板ピン設定
 
 // UART
-const int SerialTX = 1; // GPIO1
-const int SerialRX = 3; // GPIO3
+const int SerialTX = 33; // GPIO32
+const int SerialRX = 32; // GPIO33
 
 // I2Cピン
 const int I2C_SDA = 21; // GPIO21
@@ -16,3 +16,12 @@ const int SD_CS = 16;   // GPIO16
 const int SD_SCK = 18;  // GPIO18
 const int SD_MOSI = 23; // GPIO23
 const int SD_MISO = 19; // GPIO19
+
+// スピーカー用
+const int SPK = 13;     // GPIO13
+
+// LED
+const int LED1 = 4;     // GPIO3
+const int LED2 = 25;    // GPIO25
+
+ 
--- prototype/speaker/speaker.cpp
+++ prototype/speaker/speaker.cpp
@@ -1,10 +1,15 @@
 #include "speaker.h"
 
+
+// スピーカー用ピン設定
+#include "psd_config.h"
 void speaker_init(){
-    
+    pinMode(SPK, OUTPUT);
 }
 
-void speaker(){
+
+void speaker(float airspeed, float altitude, bool isInsideArea, float time_to_reach){
+
     static int sound_freq = 440; //サウンド周波数
     static int spk_flag = 0;
     static uint32_t speaker_last_change_time = millis();
